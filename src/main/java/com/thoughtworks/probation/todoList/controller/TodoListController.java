7de287d29b7422e3ed76afea85b7e1b643cf8d53
liaowenqiang <wenqiang.liao@liaowenqiangdeMacBook-Pro.local>
2020-11-05 15:02:52 +0800

    [Wenqiang Liao] feat: fix the CRO

--- src/main/java/com/thoughtworks/probation/todoList/controller/TodoListController.java
+++ src/main/java/com/thoughtworks/probation/todoList/controller/TodoListController.java
@@ -2,11 +2,13 @@ package com.thoughtworks.probation.todoList.controller;
 
 import com.thoughtworks.probation.todoList.dto.Event;
 import com.thoughtworks.probation.todoList.service.TodoListService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class TodoListController {
     private final TodoListService todoListService;
