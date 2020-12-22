f2caaf713e3bef9c5f60c461236777b7657b91cf
liaowenqiang <wenqiang.liao@liaowenqiangdeMacBook-Pro.local>
2020-12-22 22:18:51 +0800

    [Wenqiang] feat: add filed called gender

--- src/main/java/com/thoughtworks/probation/todoList/constant/mock_dev.java
+++ src/main/java/com/thoughtworks/probation/todoList/constant/mock_dev.java
@@ -3,4 +3,7 @@ package com.thoughtworks.probation.todoList.constant;
 public class mock_dev {
     // edit in mock-dev-2 branch
     public String name;
+
+    // edit in mock-dev-2 branch second commit
+    public String gender;
 }
